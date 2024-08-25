bca3941f87474850bc5fa4ab46bef196cac831f8
Saeed <yasrebi.saeed@gmail.com>
2024-08-25 18:47:16 +0100

    Starting from our domain

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,13 +1,45 @@
 package users
 
 import (
+	"bookstore_users-api/domain/users"
+	"bookstore_users-api/services"
+	"bookstore_users-api/utils/errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Not Implemented")
+	var user users.User
+	// One way of implementation of JSON parsing
+	// fmt.Println(user)
+	// body, err := io.ReadAll(c.Request.Body)
+	// if err != nil {
+	// 	c.String(http.StatusBadRequest, "Unable to read request body")
+	// 	return
+	// }
+	// if err := json.Unmarshal(body, &user); err != nil {
+	// 	c.String(http.StatusBadRequest, "Invalid JSON Format")
+	// 	return
+	// }
+
+	// Another way of implementation of JSON parsing
+	if err := c.ShouldBindJSON(&user); err != nil {
+		// return bad request to the caller
+		restError := errors.NewBadRequestError("Invalid JSON Request Format")
+		c.JSON(restError.Status, restError)
+		return
+	}
+
+	result, saveError := services.CreateUser(user)
+
+	if saveError != nil {
+		c.JSON(saveError.Status, saveError)
+		c.String(http.StatusInternalServerError, "Unable to save user")
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"bookstore_users-api/domain/users"
+	"bookstore_users-api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestError) {
+	return &user, nil
+}
